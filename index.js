a22086647a27fcab6b2389fafc9b9adb6be38af3
Jakhongirbek <jakhonyokubjonov@gmail.com>
2025-06-01 08:28:24 +0900

    CORS set up

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
 	cors({
 		origin: [
 			process.env.FRONTEND_URL,
-			'https://sizning-frontend-domeningiz.vercel.app',
+			'https://king-kebab-front-end.vercel.app/',
 		],
 		credentials: true,
 	})
