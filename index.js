2bd3d90ae5f324308605fdef082bf42afcb5e254
Jakhongirbek <jakhonyokubjonov@gmail.com>
2025-06-02 11:42:33 +0900

    CORS Bug

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ const app = express()
 // Security middleware
 app.use(helmet())
 app.use(express.json())
+
+// CORS configuration
 app.use(
 	cors({
-		origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+		origin: [
+			'https://king-kebab-front-end.vercel.app',
+			'http://localhost:3000',
+		],
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
+		exposedHeaders: ['Content-Range', 'X-Content-Range'],
 	})
 )
 
